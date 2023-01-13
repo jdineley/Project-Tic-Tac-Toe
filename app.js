ade6aba3bcf2039aa8003907d2bbedd7b39aa5ae
James Dineley <jdineley@hotmail.com>
2023-01-13 17:31:30 +0000

    build basic user interface

--- app.js
+++ app.js
@@ -26,11 +26,58 @@
 // [x,x,2,x,4,x,6,x,x]
 
 let squares = document.querySelectorAll('.square');
-let squaresArr = [...squares];
 let result = document.querySelector('.result');
+let player = document.querySelectorAll('.player div');
+let startGameButton = document.querySelector('.start-game button');
+let choosePlayer = document.querySelector('.choose-player');
+
+let squaresArr = [...squares];
 
 let gameboardArr = [];
 let curPlayer = 'X'
+
+function init() {
+    player.forEach((pl) => {
+        pl.classList.remove('active-player')
+        pl.addEventListener('click', firstPlayer)
+    })
+    startGameButton.addEventListener('click', startGame);
+    choosePlayer.style.display = 'block'
+}
+
+init();
+
+
+
+function runGame(e) {
+    // console.log(squaresArr.indexOf(e.target))
+    e.target.textContent = curPlayer;
+    checkBoard()
+    changePlayer();
+}
+
+function firstPlayer(e) {
+    e.target.classList.add('active-player');
+    player.forEach((pl) => {
+        pl.removeEventListener('click', firstPlayer)
+    })
+    // console.log(e.target.textContent)
+    curPlayer = e.target.textContent;
+    startGameButton.style.display = 'block';
+    choosePlayer.style.display = 'none';
+    result.textContent = '';
+}
+
+function startGame(){
+
+    squares.forEach((sq) => {
+        sq.addEventListener('click', runGame)
+    })
+
+    startGameButton.style.display = 'none';
+    
+}
+
 function changePlayer() {
     if(curPlayer === 'X'){
         curPlayer = 'O'
@@ -39,16 +86,16 @@ function changePlayer() {
     }
 }
 function showWinner(winner) {
+    // result.style.display = 'block'
     result.textContent = `${winner} is the winner`
 }
+
 function checkBoard(){
     document.querySelectorAll('.square').forEach((sq, i) => {
         gameboardArr[i] = sq.textContent;
     })
     // console.log(gameboardArr);
-    let winningIndices = [
-        '0,1,2','3,4,5','6,7,8','0,3,6','1,4,7','2,5,8','0,4,8','2,4,6'
-    ];
+    let winningIndices = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
     // console.log(curPlayer);
     let currentIndices = gameboardArr.map((player, index) => {
         if(player === curPlayer) {
@@ -56,23 +103,30 @@ function checkBoard(){
         }
     }).filter(el => el >= 0);
     console.log(currentIndices.join(','));
-
-    if(winningIndices.includes(currentIndices.join(','))){
-        showWinner(curPlayer);
+    for(let i = 0; i < winningIndices.length; i++){
+        if(winningIndices[i].every((e) => {
+            return currentIndices.includes(e)
+        })) {
+            showWinner(curPlayer);
+            resetBoard();
+            init();
+        }
     }
+    // if(winningIndices.includes(currentIndices.join(','))){
+    //     showWinner(curPlayer);
+    //     resetBoard();
+    //     init();
+    // }
     
+}
+function resetBoard(){
+    gameboardArr = [];
+    squares.forEach((sq) => {
+        sq.textContent = '';
+    })
 }
 
 
-// let squaresArray = Array.from(squares)
-squares.forEach((sq) => {
-    sq.addEventListener('click', function(e) {
-        console.log(squaresArr.indexOf(e.target))
-        e.target.textContent = curPlayer;
-        checkBoard()
-        changePlayer();
-    })
-})
 
 
 
